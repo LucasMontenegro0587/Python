222ded9268852e822ca017d9eb4a5058f4cf395c
Lu <117615336+LucasMontenegro0587@users.noreply.github.com>
2024-10-23 00:47:20 -0300

    passwords-generator.py

---
+++ passwords-generator.py
@@ -0,0 +1,16 @@
+import random
+import string
+
+def generar_contraseña(longitud):
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    contraseña = ''.join(random.choice(caracteres) for _ in range(longitud))
+    return contraseña
+
+def main():
+    print("**Generador de Contraseñas Seguras**")
+    longitud = int(input("introduce la longitud de la contraseña: "))
+    contraseña = generar_contraseña(longitud)
+    print(f"Tu nueva contraseña es: {contraseña}")
+
+if __name__ == "__main__":
+    main()
